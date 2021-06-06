cafa15b33f6845746935ceeb1ad9ee3ee6be1947
Dhayanandh14 <dhayanandh14@gmail.com>
2021-06-06 20:43:18 +0530

    User can now complete and update an existing todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,8 +17,9 @@ class TodosController < ApplicationController
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    response_text = "hey, Your new Todo is created with the id #{new_todo.id}"
-    render plain: response_text
+    # response_text = "hey, Your new Todo is created with the id #{new_todo.id}"
+    # render plain: response_text
+    redirect_to todos_path
   end
 
   def update
@@ -27,6 +28,8 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Updated todo completed status to #{completed}"
+    # render plain: "Updated todo completed status to #{completed}"
+    redirect_to todos_path
   end
+
 end
