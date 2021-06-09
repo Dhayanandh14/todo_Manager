52e61d1c6860c972e0b8bf1cafb9907786aa2ebc
Dhayanandh14 <dhayanandh14@gmail.com>
2021-06-09 17:12:15 +0530

    User has_secure_password to save password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
+  def new
+    render "users/new"
+  end
   def index
     render plain: User.all.map { |user| user.to_users }.join("\n") #show the user details
   end
@@ -9,17 +10,19 @@ class UsersController < ApplicationController
   def login
     email = params[:email]
     password = params[:password]
-    user = User.where("email = ? and password = ?", email, password).first
+    user = User.where("email = ? and password = ?", email, password_digest).first
     render plain: user.present?
   end
 
   #create the user given email name password
   def create
-    name = params[:name]
+    first_name = params[:first_name]
+    last_name = params[:last_name]
     email = params[:email]
     password = params[:password]
-    users = User.create!(name: name, email: email, password: password)
-    response_text = "Yeah Your Account is Created Name: #{name} Email:#{email} Password:#{password}"
-    render plain: response_text
+    users = User.create!(first_name: first_name, last_name: last_name,email: email, password: password)
+    redirect_to "/"
+    # response_text = "Yeah Your Account is Created FirstName: #{first_name}  LastName: #{last_name}  Email:#{email}"
+    # render plain: response_text
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :todos
+  has_secure_password
+
   def to_users
     "#{id} Email #{email} Password #{password}"#show the user details
   end
